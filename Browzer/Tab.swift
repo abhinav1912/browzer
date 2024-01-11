3bba4efc09397539a24b3bbaf596ea30e42105c8
abhinav1912 <abhinavmathur1912@gmail.com>
2024-01-11 23:28:05 +0530

    wip: added a basic Tab struct

--- Browzer/Tab.swift
+++ Browzer/Tab.swift
@@ -5,4 +5,9 @@ import Foundation
 struct Tab {
     var title: String
     var url: String
+
+    init(urlString: String) {
+        self.url = urlString
+        self.title = URL(string: urlString)?.host() ?? urlString
+    }
 }
