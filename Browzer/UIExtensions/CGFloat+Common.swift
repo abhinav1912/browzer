7eeeaa4d967ea2d46aa1541fda866109f1a96952
abhinav1912 <abhinavmathur1912@gmail.com>
2024-01-19 00:41:41 +0530

    feat: extension for common floats

---
+++ Browzer/UIExtensions/CGFloat+Common.swift
@@ -0,0 +1,21 @@
+//
+
+import Foundation
+
+extension CGFloat {
+
+    /// 2 points
+    static let xSmall = 2.0
+
+    /// 4 points
+    static let small = 4.0
+
+    /// 8 points
+    static let medium = 8.0
+
+    /// 12 points
+    static let large = 12.0
+
+    /// 16 points
+    static let xLarge = 16.0
+}
