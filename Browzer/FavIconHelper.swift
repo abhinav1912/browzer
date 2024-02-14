47fe2b133578a84925261e42028a06cbdd502898
abhinav1912 <abhinavmathur1912@gmail.com>
2024-02-14 22:32:50 +0530

    wip: added FavIconHelper

---
+++ Browzer/FavIconHelper.swift
@@ -0,0 +1,15 @@
+//
+
+import Foundation
+
+struct FavIconHelper {
+    enum IconSize: Int {
+        case small = 16
+        case medium = 32
+        case large = 64
+    }
+
+    static func getUrlForDomain(_ domain: String, iconSize: IconSize = .small) -> String {
+        "https://www.google.com/s2/favicons?sz=\(iconSize.rawValue)&domain=\(domain)"
+    }
+}
