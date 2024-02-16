dc9a50c40dd4c1af7a5cac3fbad293431470639c
abhinav1912 <abhinavmathur1912@gmail.com>
2024-02-16 20:10:06 +0530

    wip: added command to open history view

--- Browzer/BrowzerApp.swift
+++ Browzer/BrowzerApp.swift
@@ -11,5 +11,12 @@ struct BrowzerApp: App {
                 .frame(minWidth: 480, minHeight: 360)
         }
         .modelContainer(PersistenceController.shared.sharedModelContainer)
+        .commands {
+            CommandGroup(after: .newItem) {
+                Button("History") {
+                    // TODO: add history view
+                }.keyboardShortcut("y")
+            }
+        }
     }
 }
