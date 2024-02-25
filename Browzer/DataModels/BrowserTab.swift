e71f1a7fc7ab0d241cafe2c32ed198b417b05f58
abhinav1912 <abhinavmathur1912@gmail.com>
2024-02-25 22:13:08 +0530

    wip: alt init for tab that accepts title

--- Browzer/DataModels/BrowserTab.swift
+++ Browzer/DataModels/BrowserTab.swift
@@ -10,12 +10,17 @@ struct BrowserTab {
     var urlHost: String
     let webView: WKWebView?
 
-    init(urlString: String, webView: WKWebView?) {
+    init(urlString: String, title: String, webView: WKWebView?) {
         self.id = UUID().uuidString
         self.url = urlString
-        self.title = URL(string: urlString)?.host() ?? urlString
+        self.title = title
         self.urlHost = title
         self.webView = webView
+    }
+
+    init(urlString: String, webView: WKWebView?) {
+        let title = URL(string: urlString)?.host() ?? urlString
+        self.init(urlString: urlString, title: title, webView: webView)
     }
 
     func loadURL() {
