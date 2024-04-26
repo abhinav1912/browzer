d4778fe9de589779a56beb0ec325c979f7386765
abhinav1912 <abhinavmathur1912@gmail.com>
2024-04-26 20:27:00 +0530

    wip: added favicon path to tab

--- Browzer/DataModels/BrowserTab.swift
+++ Browzer/DataModels/BrowserTab.swift
@@ -9,18 +9,24 @@ struct BrowserTab {
     var url: String
     var urlHost: String
     let webView: WKWebView?
+    let faviconPath: String
 
-    init(urlString: String, title: String, webView: WKWebView?) {
+    init(urlString: String, title: String, webView: WKWebView?, faviconPath: String? = nil) {
         self.id = UUID().uuidString
         self.url = urlString
         self.title = title
         self.urlHost = title
         self.webView = webView
+        self.faviconPath = faviconPath ?? FavIconHelper.getUrlForDomain(urlHost)
     }
 
     init(urlString: String, webView: WKWebView?) {
         let title = URL(string: urlString)?.host() ?? urlString
         self.init(urlString: urlString, title: title, webView: webView)
+    }
+
+    init(favouritesTab: FavouritesTab, webView: WKWebView?) {
+        self.init(urlString: favouritesTab.url, webView: webView)
     }
 
     func loadURL() {
