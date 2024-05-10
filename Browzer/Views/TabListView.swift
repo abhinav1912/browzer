0d247978133874f37354ee38b84ac45016914ef4
abhinav1912 <abhinavmathur1912@gmail.com>
2024-05-10 20:27:09 +0530

    wip: refactored the tab list view to its own struct

---
+++ Browzer/Views/TabListView.swift
@@ -0,0 +1,38 @@
+//
+
+import SwiftUI
+
+struct TabListView: View {
+    let tab: BrowserTab
+    let isHovering: Bool
+
+    var body: some View {
+        HStack {
+            CachedAsyncImage(
+                url: URL(string: tab.faviconPath),
+                content: { image in
+                    image
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                },
+                placeholder: {
+                    Image(systemName: "globe")
+                }
+            )
+            Text(tab.title)
+                .lineLimit(1)
+            Spacer()
+            if isHovering {
+                Button(
+                    action: {
+                        // TODO: dismiss the tab
+                    },
+                    label: {
+                        Image(systemName: "xmark")
+                    }
+                )
+                .buttonStyle(.plain)
+            }
+        }
+    }
+}
